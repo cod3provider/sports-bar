a046b4e142c82abf38d445c29e7f89ca43b5ee20
Ilya Bondarchuk <cod3provider@gmail.com>
2023-08-12 19:52:37 +0300

    add features for open nav in header + styles

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,15 +1,81 @@
 import Logo from "../Logo/index.js";
 
-import { TbMenu2 } from "react-icons/tb";
-import {BurgerButton, BurgerIcon, StyledNav} from "./Nav.styled.js";
+import {BurgerButton, BurgerIcon, LinkNav, MobileMenu, NavList, StyledNav} from "./Nav.styled.js";
+import {useEffect, useRef, useState} from "react";
 
 const Nav = () => {
+	const [isOpenMenu, setIsOpenMenu] = useState(false);
+	const mobileMenuRef = useRef(null);
+
+	const handleMobileMenuToggle = () => {
+		setIsOpenMenu(!isOpenMenu);
+	};
+
+	useEffect(() => {
+		const handleBackdropClick = e => {
+			if (isOpenMenu &&
+				mobileMenuRef.current &&
+				!mobileMenuRef.current.contains(e.target)) {
+				setIsOpenMenu(false);
+			}
+		}
+
+		document.addEventListener('mousedown', handleBackdropClick);
+
+		return () => {
+			document.removeEventListener('mousedown', handleBackdropClick);
+		}
+	}, [isOpenMenu]);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if (isOpenMenu) {
+				setIsOpenMenu(false);
+			}
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+
+	}, [isOpenMenu]);
+
+
 	return (
 		<StyledNav>
-			<Logo />
-			<BurgerButton>
-				<BurgerIcon />
+			<Logo/>
+			<BurgerButton onClick={handleMobileMenuToggle}>
+				<BurgerIcon/>
 			</BurgerButton>
+			<MobileMenu className={isOpenMenu ? 'open' : ''} ref={mobileMenuRef}>
+				<NavList>
+					<li>
+						<LinkNav href="">Home</LinkNav>
+					</li>
+
+					<li>
+						<LinkNav href="">Menu</LinkNav>
+					</li>
+
+					<li>
+						<LinkNav href="">Events</LinkNav>
+					</li>
+
+					<li>
+						<LinkNav href="">About</LinkNav>
+					</li>
+
+					<li>
+						<LinkNav href="">Gallery</LinkNav>
+					</li>
+
+					<li>
+						<LinkNav href="">Find us</LinkNav>
+					</li>
+				</NavList>
+			</MobileMenu>
 		</StyledNav>
 	)
 }
--- src/components/Nav/Nav.styled.js
+++ src/components/Nav/Nav.styled.js
@@ -1,6 +1,7 @@
-import styled from "styled-components/macro";
+import styled, {css} from "styled-components/macro";
 import Button from "../Button/Button.jsx";
 import { TbMenu2 } from "react-icons/tb";
+// import {Link} from "react-router-dom";
 
 export const StyledNav = styled.nav`
   display: flex;
@@ -13,8 +14,8 @@ export const BurgerButton = styled(Button)`
   justify-content: center;
   align-items: center;
   padding: 0;
-  width: 40px;
-  height: 40px;
+  width: 32px;
+  height: 32px;
   background-color: transparent;
   border: none;
 `
@@ -24,4 +25,36 @@ export const BurgerIcon = styled(TbMenu2)`
   height: 32px;
   background-color: #F3E5CD;
   border-radius: 10px;
+`
+
+export const MobileMenu = styled.div`
+  display: none;
+  
+  &.open {
+    position: absolute;
+    top: 14px;
+    right: 16px;
+    display: flex;
+    justify-content: center;
+    padding: 50px;
+    width: 215px;
+    z-index: 101;
+    background-color: #BBAFB3;
+    border-radius: 10px;
+  }
+`
+
+export const NavList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`
+
+export const LinkNav = styled.a`
+  color: #504C4C;
+  font-family: 'Varela Round', sans-serif;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px; /* 114.286% */
 `
