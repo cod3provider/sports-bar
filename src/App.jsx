62271e1c7571f7f62be15ebda24765568382908b
Freasy-Vlad <vladdanilchenkodune@gmail.com>
2023-08-15 10:50:03 +0300

    add Menu and their children components
    add start components without styles and main logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import './App.css'
-import Header from "./layout/Header/index.js";
-import Main from "./layout/Main/index.js";
-import Footer from "./layout/Footer/index.js";
+import './App.css';
+import Header from './layout/Header/index.js';
+import Main from './layout/Main/index.js';
+import Footer from './layout/Footer/index.js';
+import { Menu } from './components/Menu/Menu';
 
 function App() {
-
-	return (
-		<>
-			<Header/>
-			<Main />
-			<Footer/>
-		</>
-	)
+  return (
+    <>
+      {/* <Header/>
+			<Main> */}
+      <Menu />
+      {/* </Main>
+			<Footer/> */}
+    </>
+  );
 }
 
-export default App
+export default App;
