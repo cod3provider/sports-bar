4f58f5dd5b93f5afd6cea6c1440a42df28a17c45
arboldyrev <105198179+Enjoy3325@users.noreply.github.com>
2023-08-15 17:36:17 +0300

    style tablet

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import './App.css'
-import Header from "./layout/Header/index.js";
-import Main from "./layout/Main/index.js";
-import Footer from "./layout/Footer/index.js";
+import './App.css';
+import Header from './layout/Header/index.js';
+import Main from './layout/Main/index.js';
+import Footer from './layout/Footer/index.js';
+import Hero from './components/Hero/Hero';
 
 function App() {
-
-	return (
-		<>
-			<Header/>
-			<Main />
-			<Footer/>
-		</>
-	)
+  return (
+    <>
+      <Header />
+      <Main>
+        <Hero />
+      </Main>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,31 +1,29 @@
-import './Table.module.scss'
+import { TableStyle, TableBody } from './Table.styled';
 
-const Table = () => {
-    return (
-        <>
-
-            <table className=''>
-                <thead>
-                    <tr>
-                        <th>Opening hours:</th>
-                    </tr>
-                    <tr>
-                        <th>Mon - Thu</th>
-                        <td>16:00 - 00:00</td>
-
-                    </tr>
-                    <tr>
-                        <th>Fri</th>
-                        <td>16:00 until the last client</td>
-                    </tr>
-                    <tr>
-                        <th>Sat - Sun</th>
-                        <td>10:00 until</td>
-                    </tr>
-                </thead>
-            </table>
-        </>
-    )
-}
-
-export default Table;
+export const Table = () => {
+  return (
+    <>
+      <TableStyle>
+        <thead>
+          <tr>
+            <th>Opening hours:</th>
+          </tr>
+        </thead>
+        <TableBody>
+          <tr>
+            <th>Mon - Thu</th>
+            <td>16:00 - 00:00</td>
+          </tr>
+          <tr>
+            <th>Fri</th>
+            <td>16:00 until the last client</td>
+          </tr>
+          <tr>
+            <th>Sat - Sun</th>
+            <td>10:00 until</td>
+          </tr>
+        </TableBody>
+      </TableStyle>
+    </>
+  );
+};
