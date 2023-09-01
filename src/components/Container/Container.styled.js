a4b76e36345cc37324ba450393bc47491dcc6e7c
Ilya Bondarchuk <cod3provider@gmail.com>
2023-09-01 19:20:19 +0300

    fixes styles of container

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -6,20 +6,17 @@ export const Container = styled.div`
   padding-right: 16px;
   padding-left: 16px;
 
-  @media (${theme.breakpoints.tablet}) {
+  @media (${theme.breakpoints.tablet.media}) {
     max-width: 480px;
   }
 
-  @media (${theme.breakpoints.fromTablet}) {
+  @media (${theme.breakpoints.fromTablet.media}) {
     padding-right: 40px;
     padding-left: 40px;
-  }
-
-  @media (${theme.breakpoints.fromTablet}) {
     max-width: 960px;
   }
 
-  @media (${theme.breakpoints.desktop}) {
+  @media (${theme.breakpoints.desktop.media}) {
     max-width: 1280px;
   }
 `;
