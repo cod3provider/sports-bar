86b31ea25db86f9cf32e3b22a60680e606de06e5
Ilya Bondarchuk <cod3provider@gmail.com>
2023-09-21 18:48:11 +0300

    fix styles for adaptive to Section

---
+++ src/components/FindUs/FindUs.styled.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components/macro';
+import SubscribeForm from '../common/SubscribeForm/index.js';
+
+export const TextAddress = styled.p`
+  color: #504C4C;
+  text-align: center;
+  font-family: 'Varela' ,sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+  line-height: 1.31;
+  
+  @media (1280px <= width) {
+    font-size: 26px;
+  }
+`
+
+export const SubscribeWrap = styled.div`
+  width: 278px;
+
+`
+export const FindUsSubscribe = styled(SubscribeForm)`
+  width: 278px;
+`
+
+export const InfoWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 90px;
+  
+  @media (1280px <= width) {
+    flex-direction: column;
+  }
+`
