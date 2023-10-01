ce732e24f3ee59dd50b5625d8d9da9a1da6da648
Ilya Bondarchuk <cod3provider@gmail.com>
2023-10-01 14:18:11 +0300

    add styles to Map wrapper

---
+++ src/components/Map/Map.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/macro';
+
+export const StyledGoogleMap = styled.div`
+  margin: 0 auto;
+  width: 714px;
+  height: 407px;
+  border-radius: 15px;
+  overflow: hidden;
+  box-shadow: 7px 7px 15px 0px rgba(0, 0, 0, 0.30);
+`;
