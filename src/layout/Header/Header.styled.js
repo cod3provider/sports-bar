8c980271cd4ef44a0f2caca2e5e12320c10eadb6
Ilya Bondarchuk <cod3provider@gmail.com>
2024-04-27 18:38:20 +0200

    small fix

--- src/layout/Header/Header.styled.js
+++ src/layout/Header/Header.styled.js
@@ -10,16 +10,6 @@ export const HeaderSection = styled.section`
   z-index: 1000;
 `
 
-export const HeaderSection = styled.section`
-  position: fixed;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 100%;
-  background: linear-gradient(0deg, rgba(0, 0, 0, 0.25) 0%, #504C4C 100%);
-  z-index: 1000;
-`
-
 export const HeaderStyled = styled.header`
   padding: 10px 0;
 `;
